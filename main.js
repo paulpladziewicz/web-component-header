2911a966c40ff0c619dbbca5cdd15b6697e124b1
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-07-01 06:21:55 -0400

    foundational web component code with the ability to set the title

---
+++ main.js
@@ -0,0 +1,41 @@
+
+class Header extends HTMLElement {
+    static get observedAttributes() {
+        return ['title'];
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' })
+        this.defaultTitle = 'Welcome to the Platform'
+        this.shadowRoot.innerHTML = `
+            <style>
+                @import "./web-component-styles.css";
+            </style>
+            <header>
+                <span id="title">${this.title || this.defaultTitle}</span>
+            </header>
+        `;
+    }
+
+    get title() {
+        return this.getAttribute('title');
+    }
+
+    set title(value) {
+        this.setAttribute('title', value)
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'title') {
+            this.shadowRoot.querySelector('#title').textContent = newValue;
+        }
+
+    }
+
+    resetTitle() {
+        this.title = this.defaultTitle;
+    }
+}
+
+customElements.define('platform-header', Header);
